289f8944eb7e93d62209f215f9754c09b257419a
YuxinZhang <taylorzhangyx@gmail.com>
2020-06-17 20:28:44 -0700

    add the q description for q1
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- techChanllenges/001.ThreeSumClosest.py
+++ techChanllenges/001.ThreeSumClosest.py
@@ -1,3 +1,16 @@
+# Given an array S of n integers, find three
+# integers in S such that the sum is closest
+# to a given number, target. Return the
+# difference  between the sum of the
+# three integers and the given number.
+# You may assume that each input would have exactly one solution.
+
+# Example
+# For example, given array S = {-1 2 1 -4}, and target = 1.
+# The sum that is closest to
+# the target is 2. (-1 + 2 + 1 = 2)
+# and the difference is 1.
+
 import sys
 
 
