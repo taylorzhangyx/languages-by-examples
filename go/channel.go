ea3bd2bf22f0c095dfc492ec5eb447ec93bec93d
YuxinZhang <taylorzhangyx@gmail.com>
2021-03-21 13:02:15 +0800

    experiment about the channel size
    Signed-off-by: YuxinZhang <taylorzhangyx@gmail.com>

--- go/channel.go
+++ go/channel.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello")
+	fmt.Print("start\n")
+
+	dataStream := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Printf("data inserted: %v\n", i)
+			dataStream <- i
+		}
+		close(dataStream)
+	}()
+
+	for v := range dataStream {
+		fmt.Printf("value received: %v\n", v)
+	}
 }
